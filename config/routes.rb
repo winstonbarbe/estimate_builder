7dd95784221b5fc479cbfbbed169f12212ccff4e
winstonbarbe <winston.barbe@gmail.com>
2025-01-09 09:43:36 -0500

    adds a few routes for orders

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/orders" => "orders#index"
+    post "/orders" => "orders#create"
+    get "/orders/:id" => "orders#show"
+  end
 end
