8c4f5fbcaf0557a5fb01914a3013d8c75b541cd5
winstonbarbe <winston.barbe@gmail.com>
2025-01-10 13:26:36 -0500

    adds seeds && edits methods to begin working through create functionality in my api

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,13 @@
 class Order < ApplicationRecord
+  def generate_eb_number
+    number = Order.last.eb_number || "EB0000"
+    return number if number == "EB0000"
+    number = number[2..-1].to_i + 1
+    number = "EB" + number.to_s.rjust(4, "0")
+    number
+  end
+
+  # def generate_name
+  #   "#{artist} - #{title}"
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# I want to seed some data for testing purposes
+Order.create(
+  eb_number: "EB0001",
+  artist: "Neil Young",
+  title: "Duma",
+  catalog_number: "CAT001",
+)
+Order.create(
+  eb_number: "EB0002",
+  artist: "The Beach Boys",
+  title: "Smiley Smile",
+  catalog_number: "CAT001",
+)
+Order.create(
+  eb_number: "EB0003",
+  artist: "The Beatles",
+  title: "Revolver",
+  catalog_number: "CAT001",
+)
